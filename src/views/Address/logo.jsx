4d0b77d3a841ccb78b54354356dc731a42b13bf6
Tatsuya167 <clark.taka16@gmail.com>
2021-02-20 10:56:34 +0900

    add some style for footer

---
+++ src/views/Address/logo.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Logo = () => {
+    return(
+        <div>
+            <svg width="141" height="115" viewBox="0 0 141 115" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M52.3515 87.0827L91.1729 19.8421L129.994 87.0827H52.3515Z" fill="#A96666" stroke="black" stroke-width="5"/>
+                <path d="M32.6297 65.0846L67.3196 5L102.009 65.0846H32.6297Z" fill="#A96666" stroke="black" stroke-width="5"/>
+                <path d="M4.33007 77.8064L40.8562 14.5413L77.3822 77.8064H4.33007Z" fill="#A96666" stroke="black" stroke-width="5"/>
+                <line x1="73.1503" y1="69.2895" x2="28.624" y2="69.2895" stroke="black" stroke-width="5"/>
+                <line x1="53.0467" y1="29.8837" x2="30.7835" y2="68.0491" stroke="black" stroke-width="5"/>
+                <line x1="85.9224" y1="28.8043" x2="56.2382" y2="80.7516" stroke="black" stroke-width="5"/>
+                <path d="M52.3515 87.0827L91.1729 19.8421L129.994 87.0827H52.3515Z" fill="#A96666" stroke="black" stroke-width="5"/>
+                <path d="M32.6297 65.0846L67.3196 5L102.009 65.0846H32.6297Z" fill="#A96666" stroke="black" stroke-width="5"/>
+                <path d="M4.33007 77.8064L40.8562 14.5413L77.3822 77.8064H4.33007Z" fill="#A96666" stroke="black" stroke-width="5"/>
+                <line x1="73.1503" y1="68.2895" x2="28.624" y2="68.2895" stroke="black" stroke-width="3"/>
+                <line x1="53.0467" y1="29.8837" x2="30.7835" y2="68.0491" stroke="black" stroke-width="5"/>
+                <line x1="85.9224" y1="28.8043" x2="56.2382" y2="80.7516" stroke="black" stroke-width="5"/>
+                <path d="M73 65.25L69 58.25L83.5 33L102 65L73 65.25Z" fill="#C4C4C4"/>
+                <path d="M67.75 61L71 66.75H64.25L67.75 61Z" fill="#F2C94C"/>
+                <path d="M31 67.5L51.25 32.75L66.25 58.5L61.25 67.5H31Z" fill="#C4C4C4"/>
+                <path d="M72.5 69.25H62.75L58 77.75H77.25L72.5 69.25Z" fill="#C4C4C4"/>
+            </svg>
+        </div>
+    )
+}
+
+export default Logo;
