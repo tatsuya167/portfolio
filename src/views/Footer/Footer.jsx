8b5bf7182245bef370007c70f6d445fc3e03590e
Tatsuya167 <clark.taka16@gmail.com>
2021-02-22 14:14:44 +0900

    Footer part was done

--- src/views/Footer/Footer.jsx
+++ src/views/Footer/Footer.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import "./Footer.scss"
 
 const Footer = () => {
     return(
         <section id="footer">
-            <div>
+            <div className="foot-container">
                 <p>Copyright &copy; 2021 Tatsuya Takarabe All Rights Reserved</p>
             </div>
         </section>
