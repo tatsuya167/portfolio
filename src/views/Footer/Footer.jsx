9395244ee75e52bbc3b8d2d9a9f75acfe029df45
Tatsuya167 <clark.taka16@gmail.com>
2021-02-22 13:52:22 +0900

    Add Footer

---
+++ src/views/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+    return(
+        <section id="footer">
+            <div>
+                <p>Copyright &copy; 2021 Tatsuya Takarabe All Rights Reserved</p>
+            </div>
+        </section>
+    )
+
+}
+
+export default Footer;
